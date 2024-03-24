ac87bd47696a2661d81f7e45a1107e0ec43169d4
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 13:48:31 +0200

    ex6 was done

--- CSharp15_Dilagate_Event_Records__24_03/Program.cs
+++ CSharp15_Dilagate_Event_Records__24_03/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace CSharp15_Dilagate_Event_Records__24_03
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static int CountSentences(this string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (c == '.' || c == '!' || c == '?')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("C#");
+            string text = "Hello! How are you? I hope you're doing well.";
+            int sentenceCount = text.CountSentences();
+            Console.WriteLine($"The number of sentences in the text is: {sentenceCount}");
         }
     }
 }
