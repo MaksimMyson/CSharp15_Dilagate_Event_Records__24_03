d3d9c4e021315e7b12b92a4a677a2e67742b2a6c
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 14:02:39 +0200

    ex3 was done

--- CSharp15_Dilagate_Event_Records__24_03/Program.cs
+++ CSharp15_Dilagate_Event_Records__24_03/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace CSharp15_Dilagate_Event_Records__24_03
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static int LengthOfLastWord(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return 0;
+
+            
+            string[] words = str.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            
+            if (words.Length == 0)
+                return 0;
+
+            
+            return words[words.Length - 1].Length;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("C#");
+            string testString = "This is a test string for counting the length of the last word.";
+
+            int lengthOfLastWord = testString.LengthOfLastWord();
+            Console.WriteLine($"The length of the last word in the string is: {lengthOfLastWord}");
         }
     }
 }
