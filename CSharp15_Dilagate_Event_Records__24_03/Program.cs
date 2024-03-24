2975e735b28864cbebb8911648f529e304194185
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 13:41:18 +0200

    ex4 was done

--- CSharp15_Dilagate_Event_Records__24_03/Program.cs
+++ CSharp15_Dilagate_Event_Records__24_03/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace CSharp15_Dilagate_Event_Records__24_03
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static int CountVowels(this string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if ("aeiouAEIOU".Contains(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("C#");
+            string text = "Hello, how are you?";
+            int vowelCount = text.CountVowels();
+            Console.WriteLine($"The number of vowels in the text is: {vowelCount}");
         }
     }
 }
