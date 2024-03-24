5b45f8a9554eca936e8ea2d91c8134252ce8658e
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 13:39:41 +0200

    ex3 was done

--- CSharp15_Dilagate_Event_Records__24_03/Program.cs
+++ CSharp15_Dilagate_Event_Records__24_03/Program.cs
@@ -1,10 +1,43 @@
 ﻿namespace CSharp15_Dilagate_Event_Records__24_03
 {
-    internal class Program
+    public static class IntExtensions
+    {
+        public static bool IsPrime(this int number)
+        {
+            if (number <= 1)
+                return false;
+
+            if (number == 2)
+                return true;
+
+            if (number % 2 == 0)
+                return false;
+
+            int sqrt = (int)Math.Sqrt(number);
+            for (int i = 3; i <= sqrt; i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("C#");
+            int numberToCheck = 17;
+
+            if (numberToCheck.IsPrime())
+            {
+                Console.WriteLine($"{numberToCheck} is prime.");
+            }
+            else
+            {
+                Console.WriteLine($"{numberToCheck} is not prime.");
+            }
         }
     }
 }
