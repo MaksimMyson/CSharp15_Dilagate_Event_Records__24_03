9dd7f46c5d872125162210befe7982b7269d629a
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 13:59:20 +0200

    ex1  was done

--- CSharp15_Dilagate_Event_Records__24_03/Program.cs
+++ CSharp15_Dilagate_Event_Records__24_03/Program.cs
@@ -1,10 +1,39 @@
 ﻿namespace CSharp15_Dilagate_Event_Records__24_03
 {
-    internal class Program
+    public static class IntExtensions
+    {
+        public static bool IsFibonacci(this int number)
+        {
+            if (number < 0)
+                return false;
+
+            int a = 0, b = 1;
+
+            while (b < number)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return b == number;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("C#");
+            int testNumber = 13;
+
+            if (testNumber.IsFibonacci())
+            {
+                Console.WriteLine($"{testNumber} is a Fibonacci number.");
+            }
+            else
+            {
+                Console.WriteLine($"{testNumber} is not a Fibonacci number.");
+            }
         }
     }
 }
