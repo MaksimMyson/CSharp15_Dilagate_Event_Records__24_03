c4bdb9a346651e51b83068c7dfe26b2d772abaf6
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 14:00:40 +0200

    ex2 was done

--- CSharp15_Dilagate_Event_Records__24_03/Program.cs
+++ CSharp15_Dilagate_Event_Records__24_03/Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace CSharp15_Dilagate_Event_Records__24_03
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static int WordCount(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return 0;
+
+            string[] words = str.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("C#");
+            string testString = "This is a test string for word counting.";
+
+            int wordCount = testString.WordCount();
+            Console.WriteLine($"The number of words in the string is: {wordCount}");
         }
     }
 }
