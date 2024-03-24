48ba6b65f04f555ec369e84cd3b2e1aca91c0d03
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 13:43:19 +0200

    ex5 was done

--- CSharp15_Dilagate_Event_Records__24_03/Program.cs
+++ CSharp15_Dilagate_Event_Records__24_03/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace CSharp15_Dilagate_Event_Records__24_03
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static int CountConsonants(this string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c) && !"aeiouAEIOU".Contains(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("C#");
+            string text = "Hello, how are you?";
+            int consonantCount = text.CountConsonants();
+            Console.WriteLine($"The number of consonants in the text is: {consonantCount}");
         }
     }
 }
