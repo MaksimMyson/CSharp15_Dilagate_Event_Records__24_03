c08dd767e79dbc802b42d352cb3d2fb6c7f31dc3
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 14:05:01 +0200

    ex4 was done

--- CSharp15_Dilagate_Event_Records__24_03/Program.cs
+++ CSharp15_Dilagate_Event_Records__24_03/Program.cs
@@ -1,10 +1,55 @@
 ﻿namespace CSharp15_Dilagate_Event_Records__24_03
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static bool IsValidParentheses(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return true; 
+
+            Dictionary<char, char> bracketsMap = new Dictionary<char, char>()
+        {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' }
+        };
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in str)
+            {
+                if (bracketsMap.ContainsKey(c)) 
+                {
+                    stack.Push(c);
+                }
+                else if (bracketsMap.ContainsValue(c)) 
+                {
+                    if (stack.Count == 0 || bracketsMap[stack.Pop()] != c)
+                    {
+                        return false; 
+                    }
+                }
+            }
+
+            return stack.Count == 0; 
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("C#");
+            string validString1 = "{}[]";
+            string validString2 = "(())";
+            string validString3 = "[{}]";
+            string invalidString1 = "[}";
+            string invalidString2 = "[[{]}]";
+
+            Console.WriteLine($"Is '{validString1}' valid? {validString1.IsValidParentheses()}");
+            Console.WriteLine($"Is '{validString2}' valid? {validString2.IsValidParentheses()}");
+            Console.WriteLine($"Is '{validString3}' valid? {validString3.IsValidParentheses()}");
+            Console.WriteLine($"Is '{invalidString1}' valid? {invalidString1.IsValidParentheses()}");
+            Console.WriteLine($"Is '{invalidString2}' valid? {invalidString2.IsValidParentheses()}");
         }
     }
 }
